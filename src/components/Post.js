7615f84f17f94df271be1e6027b23f8d8e0f0a24
@douglasabnovato <douglasabnovato@gmail.com>
2019-06-04 20:00:34 -0300

    reactjs

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,26 +6,40 @@ export default class Post extends React.Component {
         super(props);
 
         this.state = {
-            comments: [
-                { text : 'Bom post'}
-            ]
+            comments: [],
+            newCommentText: ''
         };
 
         this.hundleSubmit = this.hundleSubmit.bind(this);
+        this.handleTextChange = this.hundleTextChange.bind(this);   
     }
 
     hundleSubmit(e){
-        //this.setState
+        this.setState({
+            comments: [
+                ...this.state.comments,
+                { text: this.state.newCommentText }
+            ]
+        });
+
+        this.setState({ newCommentText: '' });
+
         e.preventDefault();
     }
     
+    hundleTextChange(e){
+        this.setState({ newCommentText: e.target.value })
+    }
     
     render(){
         return (
             <div>
-                <h2>{this.props.title}</h2>
+                <h1>{this.props.title}</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <input />
+                    <input 
+                        value={this.state.newCommentText}
+                        onChange={this.handleTextChange}
+                        />
                     <button type="submit">Comentário</button>
                 </form>
                 { this.state.comments.map((comment, index) => {
